c7083f575e3ad4d9f459a641b0ddc1ed024fb399
lijie <317067287@qq.com>
2022-06-13 17:21:43 +0800

    项目初始结构创建

---
+++ gulimall-wares/src/main/java/com/atguigu/gulimall/wares/GulimallWaresApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.wares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWaresApplication.class, args);
+    }
+
+}
---
+++ gulimall-wares/src/test/java/com/atguigu/gulimall/wares/GulimallWaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.wares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
